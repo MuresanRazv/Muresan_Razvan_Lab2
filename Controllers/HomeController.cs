be82191fbca0dbe75bce8a77691441db29044772
MuresanRazv <razvy9099@yahoo.com>
2024-11-29 18:05:26 +0200

    finished lab 7

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Muresan_Razvan_Lab2.Controllers
             return View();
         }
 
+        public IActionResult Chat()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
--- Program.cs
+++ Program.cs
@@ -2,10 +2,13 @@
 using Microsoft.Extensions.DependencyInjection;
 using LibraryModel.Data;
 using Muresan_Razvan_Lab2.Data;
+using Muresan_Razvan_Lab2.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<LibraryContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryContext") ?? throw new InvalidOperationException("Connection string 'LibraryContext' not found."), b => b.MigrationsAssembly("Muresan_Razvan_Lab2")));
+
+builder.Services.AddSignalR();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -36,5 +39,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapHub<ChatHub>("/Chat");
 
 app.Run();
